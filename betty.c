3cffcc063d5069bd29b30d53ef2da456e775f542
Livingstone <livingstonebarasa05@gmail.com>
2023-11-04 05:41:20 -0700

    pass betty checks

---
+++ betty.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+    char *message = "Hello, Betty!"; /* Use meaningful variable names */
+
+    printf("%s\n", message);
+
+    return (0);
+}
+
